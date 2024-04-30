42e9ee2380df1c96cb6771dfee6f9e3a40010784
AaliyahSorto <156110259+AaliyahSorto@users.noreply.github.com>
2024-04-30 07:33:04 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,57 +1,37 @@
 namespace SpriteKind {
     export const Crashpart = SpriteKind.create()
+    export const power = SpriteKind.create()
+    export const Powerups = SpriteKind.create()
+    export const Part = SpriteKind.create()
+}
+function whattheFunction (numParts: number) {
+    daLocations2 = tiles.getTilesByType(assets.tile`myTile3`)
+    if (numParts > daLocations2.length) {
+        numParts = 0
+    }
+    for (let index = 0; index < numParts; index++) {
+        partsss2 = sprites.create(crashParts._pickRandom(), SpriteKind.Powerups)
+        tiles.placeOnTile(partsss2, daLocations2.removeAt(randint(0, daLocations2.length)))
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    if (info.score() == 0) {
+        info.changeScoreBy(1)
+    } else {
+        info.changeScoreBy(-1)
+    }
     scene.cameraShake(4, 1000)
     pause(1000)
     music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.UntilDone)
     sprites.destroy(otherSprite)
-    playerAnswer("0")
+    whattheFunction(1)
 })
-function checkAnswer (answer: string) {
-    if (answer != "") {
-        correctAnswer = correctAnswers[currentQuestionIndex]
-    }
-    if (answer == correctAnswer) {
-        game.splash("Good Job!")
-        currentQuestionIndex += 1
-    } else if (answer != correctAnswer) {
-        game.splash("Wrong! Try again.")
-    }
-    if (currentQuestionIndex < questions.length) {
-        game.splash(questions[currentQuestionIndex])
-        for (let option22 of options[currentQuestionIndex]) {
-            game.splash(option22)
-        }
-        game.splash("Pick A or B")
-        playerAnswer2 = game.askForString("", 1)
-    }
-}
-function playerAnswer (answer: string) {
-    if (answer != "") {
-        correctAnswer = correctAnswers[currentQuestionIndex]
-    }
-    if (answer == "0") {
-        game.splash("You crashed")
-    } else if (answer == correctAnswer) {
-        game.splash("Good Job!")
-        currentQuestionIndex += 1
-    } else {
-        game.splash("Wrong! Try again.")
-    }
-    if (currentQuestionIndex < questions.length) {
-        game.splash(questions[currentQuestionIndex])
-        for (let option2 of options[currentQuestionIndex]) {
-            game.splash(option2)
-        }
-        game.splash("Pick a or b")
-        playerAnswer2 = game.askForString("", 1)
-        checkAnswer(playerAnswer2)
-    }
+function areCoinsOnMap () {
+    return sprites.allOfKind(SpriteKind.Food).length > 0
 }
 function coinplacement () {
-    for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
-        tiles.placeOnTile(coin, value)
+    for (let value3 of tiles.getTilesByType(assets.tile`myTile2`)) {
+        tiles.placeOnTile(coin, value3)
         coin = sprites.create(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -77,7 +57,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
     info.changeScoreBy(1)
 })
 function Quickintro () {
-    story.printCharacterText("Collect the coins and watch out for the cars! Answer the questions.", "Welcome to GetURLicense")
+    story.printCharacterText("Collect the coins and watch out for the cars!Hope you enjoy GetURLicense :)", "Welcome to GetURLicense")
     pause(1000)
     game.splash("Are you ready?")
     story.showPlayerChoices("Yes", "Maybe")
@@ -96,19 +76,10 @@ let movingroadside2: Sprite = null
 let movingroadside: Sprite = null
 let crashcar1: Sprite = null
 let coin: Sprite = null
-let playerAnswer2 = ""
-let correctAnswer = ""
-let correctAnswers: string[] = []
-let options: string[][] = []
-let questions: string[] = []
-let currentQuestionIndex = 0
-let playerAnswer23 = ""
-let correctAnswer2 = ""
-let playerAnswer22 = ""
-currentQuestionIndex = 0
-questions = ["What is the shape of a stop sign?", "Who may use shared lanes?", "How often must motor vehicles be inspected?"]
-options = [["a)Octagon", "b)Circle"], ["a)Bicyclist", "b)Motorcyclist"], ["a)Once a year", "b)Once every two years "]]
-correctAnswers = ["a", "a", "a"]
+let partsss2: Sprite = null
+let numParts = 0
+let daLocations2: tiles.Location[] = []
+let crashParts: Image[] = []
 let playerscar = sprites.create(img`
     . . . . . . a a c c a a . . . . 
     . . . . . a 3 3 3 3 3 3 a . . . 
@@ -130,6 +101,58 @@ let playerscar = sprites.create(img`
 playerscar.setPosition(56, 95)
 playerscar.setStayInScreen(true)
 controller.moveSprite(playerscar, 100, 100)
+crashParts = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . d d d d . . . . . . . . . 
+    . . d d b b d d . . . . . . . . 
+    . . d b b b b d d d . . . . . . 
+    . . d b b b b b b d d d . . . . 
+    . . d b b b b b b b b d . . . . 
+    . . d b b b b b b b b d . . . . 
+    . . d b b b b b b b b d d . . . 
+    . . d d b b b b b b b b d . . . 
+    . . . d d b b b b b b b d . . . 
+    . . . . d d d b b b b b d . . . 
+    . . . . . . d b b b b b d . . . 
+    . . . . . . d d d d b b d . . . 
+    . . . . . . . . . d d d d . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . 8 8 8 8 . . . . . . . . . 
+    . . 8 8 6 6 8 8 . . . . . . . . 
+    . . 8 6 6 6 6 8 8 8 . . . . . . 
+    . . 8 6 6 6 6 6 6 8 8 8 . . . . 
+    . . 8 6 6 6 6 6 6 6 6 8 . . . . 
+    . . 8 6 6 6 6 6 6 6 6 8 . . . . 
+    . . 8 6 6 6 6 6 6 6 6 8 8 . . . 
+    . . 8 8 6 6 6 6 6 6 6 6 8 . . . 
+    . . . 8 8 6 6 6 6 6 6 6 8 . . . 
+    . . . . 8 8 8 6 6 6 6 6 8 . . . 
+    . . . . . . 8 6 6 6 6 6 8 . . . 
+    . . . . . . 8 8 8 8 6 6 8 . . . 
+    . . . . . . . . . 8 8 8 8 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . 4 4 4 4 . . . . . . . . . 
+    . . 4 4 2 2 4 4 . . . . . . . . 
+    . . 4 2 2 2 2 4 4 4 . . . . . . 
+    . . 4 2 2 2 2 2 2 4 4 4 . . . . 
+    . . 4 2 2 2 2 2 2 2 2 4 . . . . 
+    . . 4 2 2 2 2 2 2 2 2 4 . . . . 
+    . . 4 2 2 2 2 2 2 2 2 4 4 . . . 
+    . . 4 4 2 2 2 2 2 2 2 2 4 . . . 
+    . . . 4 4 2 2 2 2 2 2 2 4 . . . 
+    . . . . 4 4 4 2 2 2 2 2 4 . . . 
+    . . . . . . 4 2 2 2 2 2 4 . . . 
+    . . . . . . 4 4 4 4 2 2 4 . . . 
+    . . . . . . . . . 4 4 4 4 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
 tiles.setCurrentTilemap(tilemap`level6`)
 info.setScore(0)
 Quickintro()
@@ -207,87 +230,9 @@ game.onUpdateInterval(4000, function () {
     crashcar1.setVelocity(0, 30)
 })
 forever(function () {
-    if (info.score() == 12) {
+    if (!(areCoinsOnMap())) {
         coinplacement()
     }
-    if (info.score() == 24) {
-        coinplacement()
-    }
-    if (info.score() == 36) {
-        coinplacement()
-    }
-    if (info.score() == 48) {
-        coinplacement()
-    }
-    if (info.score() == 60) {
-        coinplacement()
-    }
-    if (info.score() == 72) {
-        coinplacement()
-        game.splash("high score")
-    }
-})
-forever(function () {
-    if (info.score() == 96) {
-        coinplacement()
-    }
-    if (info.score() == 108) {
-        coinplacement()
-    }
-    if (info.score() == 120) {
-        coinplacement()
-    }
-    if (info.score() == 132) {
-        coinplacement()
-    }
-    if (info.score() == 144) {
-        coinplacement()
-    }
-    if (info.score() == 156) {
-        coinplacement()
-    }
-})
-forever(function () {
-    if (info.score() == 144) {
-        coinplacement()
-    }
-    if (info.score() == 156) {
-        coinplacement()
-    }
-    if (info.score() == 168) {
-        coinplacement()
-    }
-    if (info.score() == 180) {
-        coinplacement()
-    }
-    if (info.score() == 192) {
-        coinplacement()
-    }
-    if (info.score() == 84) {
-        coinplacement()
-    }
-})
-game.onUpdateInterval(10000, function () {
-    crashcar1 = sprites.createProjectileFromSide(img`
-        . . . . . . 8 8 c c 8 8 . . . . 
-        . . . . . 8 6 6 6 6 6 6 8 . . . 
-        . . . . 6 c 6 6 6 6 6 6 c 6 . . 
-        . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
-        . . . f 6 6 9 6 6 6 6 6 c 6 f . 
-        . . . f 6 6 9 6 6 6 6 6 6 6 f . 
-        . . . f 6 6 9 6 6 6 6 6 6 6 f . 
-        . . . f 6 c 6 9 9 6 6 6 c 6 f . 
-        . . . 8 6 c 8 c c c c 8 c 6 8 . 
-        . . . 8 6 8 c b b b b c 8 6 8 . 
-        . . . 8 6 8 b b b b b b 8 6 8 . 
-        . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
-        . . . f 8 d 8 8 8 8 8 8 d 8 f . 
-        . . . f 8 6 d 8 8 8 8 d 6 8 f . 
-        . . . f f 8 8 8 8 8 8 8 8 f f . 
-        . . . . f f . . . . . . f f . . 
-        `, 0, 50)
-    crashcar1.setPosition(60, 0)
-    crashcar1.setVelocity(0, 30)
 })
 game.onUpdateInterval(10000, function () {
     crashcar1 = sprites.createProjectileFromSide(img`
@@ -353,5 +298,27 @@ game.onUpdateInterval(10000, function () {
         . . . . f f . . . . . . f f . . 
         `, 0, 50)
     crashcar1.setPosition(60, 49)
+    crashcar1.setVelocity(0, 30)
+})
+game.onUpdateInterval(10000, function () {
+    crashcar1 = sprites.createProjectileFromSide(img`
+        . . . . . . 8 8 c c 8 8 . . . . 
+        . . . . . 8 6 6 6 6 6 6 8 . . . 
+        . . . . 6 c 6 6 6 6 6 6 c 6 . . 
+        . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
+        . . . f 6 6 9 6 6 6 6 6 c 6 f . 
+        . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+        . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+        . . . f 6 c 6 9 9 6 6 6 c 6 f . 
+        . . . 8 6 c 8 c c c c 8 c 6 8 . 
+        . . . 8 6 8 c b b b b c 8 6 8 . 
+        . . . 8 6 8 b b b b b b 8 6 8 . 
+        . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . . f 8 d 8 8 8 8 8 8 d 8 f . 
+        . . . f 8 6 d 8 8 8 8 d 6 8 f . 
+        . . . f f 8 8 8 8 8 8 8 8 f f . 
+        . . . . f f . . . . . . f f . . 
+        `, 0, 50)
+    crashcar1.setPosition(60, 0)
     crashcar1.setVelocity(0, 30)
 })
